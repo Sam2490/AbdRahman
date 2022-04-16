b5268e617376b44e8755c3cef6c96a39a848e169
Sam2490 <smoilfagiry@gmail.com>
2022-04-16 14:46:33 +0200

    Edited the whole project

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,13 @@
+// Remove Loader 
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector(".spinner").style.visibility = "visible";
+    } else {
+        document.querySelector(".spinner").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
 // Toggle Menu
 const toggleBtn = document.querySelector('.toggle-btn')
 const toggleMenu = document.querySelector('.nav-left')
